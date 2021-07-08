8184e9d2b305ca2c25f0c8caa23fb429e515aad8
falconrr <44069494+falconrr@users.noreply.github.com>
2021-07-08 09:46:33 -0500

    Week2

---
+++ Week 2/Materials/segmenttestfile.py
@@ -0,0 +1,12 @@
+import shutup;shutup.please() # ignora esto por favor. No es necesario en tu código
+
+import nltk # importamos módulos
+import sys
+sentences = [] # creamos una lista vacia
+
+with open(sys.argv[1], 'r') as f: # le pedimos a python que lea el archivo que especificamos en la terminal
+    text = f.readlines() # lee línea por línea
+    for line in text: # for loop: lee línea por línea y realiza una tarea repetitivamente
+        nltk_sents = nltk.sent_tokenize(line) # la tarea es que segmente las oraciones del texto con el módulo NLTK
+        sentences.append(nltk_sents) # agrega los tokens a la lista vacía
+        print(nltk_sents) # imprime el resultado
---
+++ Week 2/Materials/tokenizetextfile.py
@@ -0,0 +1,12 @@
+import shutup;shutup.please() # ignora esto por favor. No es necesario en tu código
+
+import nltk # importamos módulos
+import sys
+tokens = [] # creamos una lista vacia
+
+with open(sys.argv[1], 'r') as f: # le pedimos a python que lea el archivo que especificamos en la terminal
+    Lines = f.readlines() # lee línea por línea
+    for line in Lines: # for loop: lee línea por línea y realiza una tarea repetitivamente
+        nltk_tokens = nltk.word_tokenize(line) # la tarea es que tokenice el texto con el módulo NLTK
+        tokens.append(nltk_tokens) # agrega los tokens a la lista vacía
+        print(nltk_tokens) # imprime el resultado
